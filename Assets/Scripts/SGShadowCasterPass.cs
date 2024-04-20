ad14d5e72ec3e46fb9ed36585f872cf159353ff0
李剑峰 <330695130@qq.com>
2024-04-20 17:36:12 +0800

    1

--- Assets/Scripts/SGShadowCasterPass.cs
+++ Assets/Scripts/SGShadowCasterPass.cs
@@ -11,14 +11,15 @@ public class SGShadowCasterPass : ScriptableRenderPass
     
     class PassData
     {
-        public TextureHandle shadowmapTexture;
+        public int Data;
     }
     public SGShadowCasterPass()
     {
         base.profilingSampler = new ProfilingSampler(nameof(SGShadowCasterPass));
         m_ForceShadowPointSampling = SystemInfo.graphicsDeviceType == GraphicsDeviceType.Metal &&
                                      GraphicsSettings.HasShaderDefine(Graphics.activeTier, BuiltinShaderDefine.UNITY_METAL_SHADOWS_USE_POINT_FILTERING);
-
+        renderPassEvent = RenderPassEvent.BeforeRenderingShadows;
+        
     }
 
     
@@ -31,49 +32,46 @@ public class SGShadowCasterPass : ScriptableRenderPass
         /*ShadowUtils.ShadowRTReAllocateIfNeeded( ref passData.shadowmapTexture, shadowData.mainLightShadowmapWidth,
             shadowData.mainLightShadowmapHeight, k_ShadowmapBufferBits, "_MainLightShadowmapTexture");
             */
+        UniversalRenderingData renderingData = frameData.Get<UniversalRenderingData>();
+        UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
+        UniversalLightData lightData = frameData.Get<UniversalLightData>();
+        UniversalShadowData shadowData = frameData.Get<UniversalShadowData>();
+        var resourceData = frameData.Get<UniversalResourceData>();
+        int shadowLightIndex = lightData.mainLightIndex;
+        if (shadowLightIndex == -1)
+            return;
         
         using (var builder=renderGraph.AddRasterRenderPass<PassData>(nameof(SGShadowCasterPass),out var passData,base.profilingSampler))
         {
-            UniversalRenderingData universalRenderingData = frameData.Get<UniversalRenderingData>();
-            UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
-            UniversalLightData lightData = frameData.Get<UniversalLightData>();
-            UniversalShadowData shadowData = frameData.Get<UniversalShadowData>();
- 
+           
+            
                        var shadowTexDescrip = new TextureDesc
             {
-                anisoLevel = 1,
-                autoGenerateMips = true,
-                bindTextureMS = false,//
-                clearBuffer = false,//
-                clearColor = Color.black,
-                colorFormat = GraphicsFormat.None,
-                depthBufferBits =DepthBits.Depth24 ,
+                clearBuffer = true,//
+                clearColor = Color.clear,
+                depthBufferBits =DepthBits.Depth16 ,
                 dimension = TextureDimension.Tex2D,
-                disableFallBackToImportedTexture = false,//
-                discardBuffer = false,
-                enableRandomWrite = false,
-                fastMemoryDesc = new FastMemoryDesc(),
                 filterMode =  m_ForceShadowPointSampling? FilterMode.Point: FilterMode.Bilinear,
-                func = null,
                 width = shadowData.mainLightShadowmapWidth,
                 height = shadowData.mainLightShadowmapHeight,
-                isShadowMap = true,
-                memoryless = RenderTextureMemoryless.None,
-                mipMapBias = 0,
                 msaaSamples = MSAASamples.None,
-                name = "_SGShadowmapTexture",
-                scale = default,
+                isShadowMap = true,
+                name = "SGShadowmapTexture",
                 sizeMode=TextureSizeMode.Explicit,
                 slices = 1,
-                useDynamicScale = false,
-                useDynamicScaleExplicit = false,
-                useMipMap = false,
                 wrapMode = TextureWrapMode.Clamp,
             };
-             
-            passData.shadowmapTexture=renderGraph.CreateTexture(shadowTexDescrip);
-         
+            
+            var shadowmapTexture=renderGraph.CreateTexture(shadowTexDescrip);
+        //    builder.SetRenderAttachment(resourceData.activeColorTexture,0);
+        var settings = new ShadowDrawingSettings(renderingData.cullResults, shadowLightIndex);
+        settings.useRenderingLayerMaskTest = UniversalRenderPipeline.asset.useRenderingLayers;
+     //   renderGraph.CreateShadowRendererList(ref settings);
+            builder.SetRenderAttachmentDepth(shadowmapTexture,0);
+         //   builder.AllowPassCulling(false); 
+        //   builder.AllowGlobalStateModification(true);
             builder.SetRenderFunc<PassData>(ExecutePass);
+            
         }
     }
 }
